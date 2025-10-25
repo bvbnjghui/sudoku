15ca395f6b2508973454d32bb9d5635dcf620fb3
Linsly <bvbnjghui@yahoo.com.tw>
2025-10-26 01:40:24 +0800

    feat(ui): center difficulty selection menu

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ function sudokuGame() {
         highlightedNumberCells: [],
         hintsUsed: 0,
         maxHints: 3, // 遊戲中實際的提示上限
-        version: '1.4.4',
+        version: '1.4.5',
         remainingCounts: {}, 
         updateAvailable: false,
         registration: null,
